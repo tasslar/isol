330a57080b8c7323a3de0cd5763282f0c126f89c
senthillar <tnav077346@gmail.com>
2017-04-16 17:05:19 +0530

    wqeqw

--- footer.php
+++ footer.php
@@ -142,7 +142,7 @@
                                 </div>
                        
                             <div class="row">
-                                <div class="col-xs-1 col-sm-1"><i class="fa fa-mobile" aria-hidden="true"></i></div>
+                                <div class="col-xs-1 col-sm-1"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
                                 <div class="col-xs-11 col-sm-11" style="font-family:Arial, Helvetica, sans-serif"><p>+1 (858) 224 5534</p> </div>
                             </div>
                             <div class="row">
