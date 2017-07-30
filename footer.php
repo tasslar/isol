e87e7874d3cc444b3a2bc9add35b301aeb1d39f2
tasslar <senthil.itbs1723@gmail.com>
2017-07-30 13:17:27 +0530

    were

--- footer.php
+++ footer.php
@@ -217,6 +217,47 @@
                                 <div class="col-xs-11 col-sm-11" style="text-indent:-9999px;">. </div>
                             </div>   
                 </div><!--/.col-md-2-->
+				
+				
+				
+				
+				 <div class="col-md-4 col-sm-6">
+                         <h3>Zone#3 Office &nbsp;<img src="images/india-flag.jpg" /></h3> 
+                         <div class="row">
+                                <div class="col-xs-1 col-sm-1"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+                                <div class="col-xs-11 col-sm-11" style="font-family:Arial, Helvetica, sans-serif">  <p>#4, Naghu nagar
+                            Bethaniapuram,Madurai 625016<br/>
+                            , India</p>
+                                </div>
+                                </div>
+                        
+                            <div class="row">
+                                <div class="col-xs-1 col-sm-1"><i class="fa fa-mobile" aria-hidden="true"></i></div>
+                                
+<div class="col-xs-1 col-sm-1"><i class="fa fa-mobile" aria-hidden="true"></i></div>
+<div class="col-xs-11 col-sm-11" style="font-family:Arial, Helvetica, sans-serif"><p>+91 95858 82159</p></div>
+
+
+                     		 </div> 
+							 
+							  <div class="row">
+                                 <div class="col-xs-1 col-sm-1"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+                                <div class="col-xs-11 col-sm-11"><a href="mailto:vijay@inysol.com" target="_blank">vijay@inysol.com</a></div>
+                            </div>
+							
+							
+                             <div class="row">
+                                <div class="col-xs-1 col-sm-1" style="text-indent:-9999px;"> .</div>
+                                <div class="col-xs-11 col-sm-11" style="text-indent:-9999px;">. </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-1 col-sm-1" style="text-indent:-9999px;"> .</div>
+                                <div class="col-xs-11 col-sm-11" style="text-indent:-9999px;">. </div>
+                            </div>   
+                </div><!--/.col-md-2-->
+				
+				
+				
                 
                  
             </div>
