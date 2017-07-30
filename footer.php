f44c8ba1f730d400041561406324466d0d444466
tasslar <senthil.itbs1723@gmail.com>
2017-07-30 13:19:48 +0530

    qee

--- footer.php
+++ footer.php
@@ -232,7 +232,7 @@
                                 </div>
                         
                             <div class="row">
-                                <div class="col-xs-1 col-sm-1"><i class="fa fa-mobile" aria-hidden="true"></i></div>
+                                 
                                 
 <div class="col-xs-1 col-sm-1"><i class="fa fa-mobile" aria-hidden="true"></i></div>
 <div class="col-xs-11 col-sm-11" style="font-family:Arial, Helvetica, sans-serif"><p>+91 95858 82159</p></div>
